4f3a7ce7c964c785bb54b38a6150d23428bcdb64
burhanuddinbaig <burhan.uddin.baig1@gmail.com>
2022-12-14 18:04:15 +0500

    sort hashrate data

--- controller/hashrateController.js
+++ controller/hashrateController.js
@@ -142,7 +142,7 @@ const getdata = async (req, res, next) => {
   try {
     const start = getStartTime(placeholder);
     //   await db.connect();
-    const history = await hashrateModel.find({ timestamp: { $gte: start } });
+    const history = await hashrateModel.find({ timestamp: { $gte: start } }).sort({ hashrate: 'asc' });
 
     body = {
       ltc_usd: ltc_usd,
